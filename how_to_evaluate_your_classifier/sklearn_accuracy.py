d7f622bb9b89d2139bfd1315f2e1e9b4f8767f3a
Yoshi Gao <yoshi4868686@gmail.com>
2022-12-18 22:49:19 +0800

    new

--- how_to_evaluate_your_classifier/sklearn_accuracy.py
+++ how_to_evaluate_your_classifier/sklearn_accuracy.py
@@ -1,7 +1,8 @@
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
 
 prediction = [0, 1, 0, 1]
 ground_truth = [0, 1, 1, 1]
 
-score = accuracy_score(prediction, ground_truth)
+score = f1_score(prediction, ground_truth)
+print(score)
